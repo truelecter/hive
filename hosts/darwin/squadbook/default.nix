fcd3041f2d12497730fffb543ab08ff2052461c0
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-07-02 07:35:12 +0300

    Declare builder on macbook

--- hosts/darwin/squadbook/default.nix
+++ hosts/darwin/squadbook/default.nix
@@ -25,4 +25,49 @@
   };
 
   system.stateVersion = 4;
+
+  nix = {
+    distributedBuilds = true;
+
+    extraOptions = ''
+      builders-use-substitutes = true
+    '';
+
+    buildMachines = [
+      {
+        system = "x86_64-linux";
+
+        supportedFeatures = [
+          "big-parallel"
+        ];
+
+        speedFactor = 2;
+        maxJobs = 5;
+        hostName = "nix-docker";
+
+        sshUser = "root";
+        sshKey = "/etc/nix/docker_rsa";
+      }
+      {
+        systems = [
+          "x86_64-linux"
+          # "aarch64-linux"
+        ];
+
+        supportedFeatures = [
+          "nixos-test"
+          "benchmark"
+          "kvm"
+          "big-parallel"
+        ];
+
+        speedFactor = 10;
+        maxJobs = 6;
+        hostName = "nas";
+
+        sshUser = "root";
+        sshKey = "/etc/nix/builder_nas";
+      }
+    ];
+  };
 }
