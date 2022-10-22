ccf5208b1fb70ac82bd8f6b551a6899231a15359
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-10-22 21:24:33 +0300

    Fix libcamera agin

--- overlays/libcamera-rpi.nix
+++ overlays/libcamera-rpi.nix
@@ -1,12 +1,15 @@
 final: prev: {
   libcamera-rpi = prev.libcamera.overrideAttrs (o: rec {
-    inherit (prev.sources.libcamera) pname version src;
+    pname = "libcamera-rpi";
+
+    inherit (prev.sources.libcamera) version src;
 
     buildInputs =
       o.buildInputs
       ++ [
         final.libyaml
         final.gnutls
+        final.boost
       ];
 
     mesonBuildType = "release";
