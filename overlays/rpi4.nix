e9aed661fc7c943d84b8710995a357fedd9bc8ff
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-10-23 21:05:28 +0300

    Removed octoprint and optimized a bit CI

---
+++ overlays/rpi4.nix
@@ -0,0 +1,16 @@
+final: prev: {
+  linuxPackages_rpi4 = prev.linuxPackages_rpi4.overrideAttrs (o: rec {
+    inherit (prev.sources.rpi-linux) version src;
+  });
+
+  raspberrypiWirelessFirmware = prev.raspberrypiWirelessFirmware.overrideAttrs (o: rec {
+    src = [
+      prev.sources.rpi-fw-nonfree.src
+      prev.sources.rpi-fw-bluez.src
+    ];
+  });
+
+  raspberrypifw = prev.raspberrypifw.overrideAttrs (o: rec {
+    inherit (prev.sources.rpi-fw) version src;
+  });
+}
