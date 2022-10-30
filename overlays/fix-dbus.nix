79bacde0919ecb5eb89ab52688922e9e2eeedb9b
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-10-30 18:24:00 +0200

    Workaround dbus fix

---
+++ overlays/fix-dbus.nix
@@ -0,0 +1,10 @@
+self: super: {
+  python3Packages = super.python3Packages.override {
+    overrides = pfinal: pprev: {
+      dbus-next = pprev.dbus-next.overridePythonAttrs (old: {
+        # temporary fix for https://github.com/NixOS/nixpkgs/issues/197408
+        checkPhase = builtins.replaceStrings ["not test_peer_interface"] ["not test_peer_interface and not test_tcp_connection_with_forwarding"] old.checkPhase;
+      });
+    };
+  };
+}
