d7f627d233152032b13d38fbfa376ece5cba41e9
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2022-11-26 18:10:34 +0200

    Enable terraform prefillRequiredFields

--- profiles/user/dev/terraform.nix
+++ profiles/user/dev/terraform.nix
@@ -11,6 +11,7 @@
       "editor.formatOnSave" = true;
     };
     "terraform.experimentalFeatures.validateOnSave" = true;
+    "terraform.experimentalFeatures.prefillRequiredFields" = true;
     "terraform.languageServer.path" = "${pkgs.terraform-ls}/bin/terraform-ls";
     "terraform.languageServer.terraform.path" = "${pkgs.terraform}/bin/terraform";
     "terraform.codelens.referenceCount" = true;
