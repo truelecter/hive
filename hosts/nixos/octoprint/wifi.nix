08ae60514dab9e293761e575976d2a416920dde0
Andrii Panasiuk <andrii.panasiuk@ringteam.com>
2023-03-14 12:44:12 +0200

    Few fixes to klipper configs

--- hosts/nixos/octoprint/wifi.nix
+++ hosts/nixos/octoprint/wifi.nix
@@ -6,7 +6,14 @@
   networking.wireless = {
     enable = true;
     interfaces = ["wlan0"];
-    networks."Xata290.5".psk = "@WIFI_PASSWORD@";
+    networks."Xata290" = {
+      psk = "@WIFI_PASSWORD@";
+      priority = 5;
+    };
+    networks."Xata290.5" = {
+      psk = "@WIFI_PASSWORD@";
+      priority = 10;
+    };
     environmentFile = config.sops.secrets.xata-password-env.path;
   };
 }
