3ca809d029e349e49c2e3f6babb4a3a9350c8a9a
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-05-31 14:01:12 +0300

    fix(nixos): reprovision hyperos box

--- cells/nixos/hosts/hyperos/_hardware-configuration.nix
+++ cells/nixos/hosts/hyperos/_hardware-configuration.nix
@@ -33,38 +33,21 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/30e9de22-7c17-4aed-8aa5-1fa715c535c0";
+    device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9EDF-27B3";
+    device = "/dev/disk/by-label/ESP";
     fsType = "vfat";
   };
 
   networking = {
-    useDHCP = false;
+    useDHCP = true;
     hostName = "hyperos";
-    networkmanager.enable = true;
+    # networkmanager.enable = true;
     firewall.enable = false;
     nameservers = ["1.1.1.1" "8.8.8.8"];
-    interfaces.eth0 = {
-      ipv4 = {
-        addresses = [
-          {
-            address = "172.16.10.100";
-            prefixLength = 24;
-          }
-        ];
-        routes = [
-          {
-            address = "0.0.0.0";
-            prefixLength = 0;
-            via = "172.16.10.254";
-          }
-        ];
-      };
-    };
   };
 
   swapDevices = [];
