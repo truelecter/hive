ccaaab9c2a0ba8a5c1f5133cf25e5de2e9655ed2
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-05-31 14:27:16 +0300

    fix(provisioning): remove assertion

--- cells/provisioning/profiles/tailscale.nix
+++ cells/provisioning/profiles/tailscale.nix
@@ -8,20 +8,19 @@
   ...
 }: let
   tailscale-key = builtins.getEnv "TL_TAILSCALE_PROVISIONING_KEY";
-in
-  assert tailscale-key != ""; {
-    services.tailscale = {
-      enable = true;
-      package = inputs.cells.common.overrides.tailscale;
-    };
+in {
+  services.tailscale = {
+    enable = true;
+    package = inputs.cells.common.overrides.tailscale;
+  };
 
-    tl.services.tailscale-autoconnect = {
-      enable = true;
-      authFile = pkgs.writeTextFile {
-        name = "tailscale-key";
-        text = tailscale-key;
-      };
+  tl.services.tailscale-autoconnect = {
+    enable = true;
+    authFile = pkgs.writeTextFile {
+      name = "tailscale-key";
+      text = tailscale-key;
     };
+  };
 
-    networking.firewall.enable = false;
-  }
+  networking.firewall.enable = false;
+}
