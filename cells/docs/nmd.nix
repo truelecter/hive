7bd6377ea4e46eb3145584e3cb78da4565cb7efe
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-07-03 12:17:10 +0300

    feat(docs): init base docs structure

--- cells/docs/nmd.nix
+++ cells/docs/nmd.nix
@@ -2,18 +2,58 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) haumea latest;
+  inherit (inputs) haumea latest nmd self;
+  inherit (inputs.cells) home;
 
   nixpkgs = import latest {inherit (inputs.nixpkgs) system;};
 
   l = inputs.nixpkgs.lib // builtins;
-in
-  l.mapAttrs (
-    _: v: nixpkgs.callPackage v {inherit cell;}
-  )
-  (
-    haumea.lib.load {
-      src = ./packages;
-      loader = haumea.lib.loaders.path;
-    }
-  )
+
+  hmModules = builtins.attrValues home.homeModules;
+
+  # Make sure the used package is scrubbed to avoid actually
+  # instantiating derivations.
+  setupModule = {
+    imports = [
+      {
+        _module.args = {
+          pkgs = l.mkForce (nmd.lib.scrubDerivations "pkgs" nixpkgs);
+          pkgs_i686 = l.mkForce {};
+        };
+        _module.check = false;
+      }
+    ];
+  };
+
+  hmModulesDocs = nmd.lib.buildModulesDocs {
+    modules =
+      hmModules
+      ++ [
+        setupModule
+      ];
+    moduleRootPaths = [../home/homeModules];
+    mkModuleUrl = path:
+      "https://gitlab.com/rycee/nur-expressions/blob/master"
+      + "/hm-modules/${path}#blob-content-holder";
+    channelName = "nur-rycee";
+    docBook = {
+      id = "nur-rycee-hm-options";
+      optionIdPrefix = "hm-opt";
+    };
+  };
+
+  docs = nmd.lib.buildDocBookDocs {
+    pathName = "nur-rycee";
+    modulesDocs = [hmModulesDocs];
+    documentsDirectory = ./documents;
+    chunkToc = ''
+      <toc>
+        <d:tocentry xmlns:d="http://docbook.org/ns/docbook" linkend="book-nur-rycee-manual"><?dbhtml filename="index.html"?>
+          <d:tocentry linkend="ch-hm-options"><?dbhtml filename="hm-options.html"?></d:tocentry>
+        </d:tocentry>
+      </toc>
+    '';
+  };
+in {
+  inherit (docs) html htmlOpenTool;
+}
