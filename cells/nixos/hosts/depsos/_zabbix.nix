d565f488e963066e35fafd0d00a5f7a12683a64f
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-08-02 16:23:06 +0300

    fix(nixos): fix zabbix server

--- cells/nixos/hosts/depsos/_zabbix.nix
+++ cells/nixos/hosts/depsos/_zabbix.nix
@@ -1,10 +1,11 @@
 {
   services.zabbixAgent = {
     enable = true;
-    server = "zabbix-px01.deps.ua";
+    server = "zabbix-px01.deps.ua,192.168.20.27";
     openFirewall = true;
     settings = {
       Hostname = "vs-kv-net-we11";
+      ServerActive = "zabbix-px01.deps.ua,192.168.20.27";
     };
   };
 }
