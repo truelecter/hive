a67864dca53a27c35b14666dc4b995da9a2984e5
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-09-05 14:35:31 +0300

    fix(ci): fix path script name

--- .ci/push-paths.sh
+++ .ci/push-paths.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cachix=$1 cachixArgs=${2:--j8} cache=$3 pathsToPush=$4 pushFilter=$5
 
 if [[ $pathsToPush == "" ]]; then
-  pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
+  pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-paths.sh))
 
   if [[ $pushFilter != "" ]]; then
     pathsToPush=$(echo "$pathsToPush" | grep -vEe "$pushFilter")
