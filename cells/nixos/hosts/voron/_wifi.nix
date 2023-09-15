e4628a7ec959169c52d522314bf02ff2fffedba1
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2023-09-15 17:47:36 +0300

    fix(nixos): create overlay for wifi to use external antenna

--- cells/nixos/hosts/voron/_wifi.nix
+++ cells/nixos/hosts/voron/_wifi.nix
@@ -23,28 +23,40 @@
   };
 
   # TODO: make it possible to choose between ant1,ant2 and noant, make PR to nixos-hardware
-  # hardware.deviceTree = {
-  #   overlays = [
-  #     # Equivalent to dtparam=audio=on
-  #     {
-  #       name = "ant2";
-  #       dtsText = ''
-  #         /dts-v1/;
-  #         /plugin/;
-  #         / {
-  #           compatible = "brcm,bcm2711";
-  #           fragment@0 {
-  #             target = <&ant2>;
+  hardware.deviceTree = {
+    overlays = [
+      # Equivalent to dtparam=audio=on
+      {
+        name = "choose-antenna";
+        dtsText = ''
+          /dts-v1/;
+          /plugin/;
+          / {
+            compatible = "brcm,bcm2711";
+            fragment@0 {
+              // Despite this being ant1 instead of ant2,
+              // CM4 will use ant2. This is because
+              // ant1 is enabled by default and switching
+              // GPI pins causes it to activate ant2
+              target = <&ant1>;
 
-  #             __overlay__ {
-  #               status = "okay";
-  #             };
-  #           };
-  #         };
-  #       '';
-  #     }
-  #   ];
-  # };
+              __overlay__ {
+                gpios = <7 0>;
+              };
+            };
+
+            fragment@1 {
+              target = <&ant2>;
+
+              __overlay__ {
+                gpios = <3 0>;
+              };
+            };
+          };
+        '';
+      }
+    ];
+  };
 
   boot = {
     extraModprobeConfig = ''
