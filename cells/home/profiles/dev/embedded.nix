9a08024839329ca44c7a7fd9e0694bb85cb6fe84
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2024-01-04 17:41:12 +0200

    chore(home): expand embedded vscode extentions

--- cells/home/profiles/dev/embedded.nix
+++ cells/home/profiles/dev/embedded.nix
@@ -13,6 +13,13 @@ in {
       rust-lang.rust-analyzer
       probe-rs.probe-rs-debugger
       zixuanwang.linkerscript
+
+      marus25.cortex-debug
+      mcu-debug.debug-tracker-vscode
+      mcu-debug.memory-view
+      mcu-debug.rtos-views
+      mcu-debug.peripheral-viewer
+      vadimcn.vscode-lldb
     ];
     # userSettings = {
     #   "rust-analyzer.cargo.target" = "thumbv7em-none-eabihf";
