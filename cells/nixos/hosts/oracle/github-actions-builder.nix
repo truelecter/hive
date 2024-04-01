0658640f6e7eb9e68ba2555ff68023a24f38b05b
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2024-04-01 13:46:09 +0300

    feat(ci): another attempt at gha with custom builder

--- cells/nixos/hosts/oracle/github-actions-builder.nix
+++ cells/nixos/hosts/oracle/github-actions-builder.nix
@@ -1,7 +1,13 @@
 {
-  users.users.root = {
+  users.users.gha-builder = {
+    isNormalUser = true;
+
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5gBk/gVG6wVBdZrtOcGu6TPZCtGCOkgZMndSA3tS7j"
     ];
+  };
+
+  nix.settings = {
+    trusted-users = ["gha-builder"];
   };
 }
