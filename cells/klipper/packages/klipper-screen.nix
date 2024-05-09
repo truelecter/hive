f06b9c356ca6b45f98025022750938f08d15319e
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2024-05-09 18:37:12 +0300

    chore(klipper): remove camera hack

--- cells/klipper/packages/klipper-screen.nix
+++ cells/klipper/packages/klipper-screen.nix
@@ -5,10 +5,4 @@
 }:
 klipperscreen.overrideAttrs (_: prev: {
   inherit (sources.klipper-screen) version src;
-
-  preFixup = ''
-    ${prev.preFixup}
-
-    cp ${./_files/camera_cfg.py} $out/dist/panels/camera_cfg.py
-  '';
 })
