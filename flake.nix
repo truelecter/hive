26216e2cb92bca7a4eb4f21d0145249d19178bec
Andrii Panasiuk <andrew.panassiouk@gmail.com>
2025-01-18 03:21:53 +0200

    fix(flake): update darwin input to release version

--- flake.nix
+++ flake.nix
@@ -90,7 +90,7 @@
     # nixos.follows = "latest";
 
     darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:LnL7/nix-darwin/nix-darwin-24.11";
       inputs.nixpkgs.follows = "nixos";
     };
 
